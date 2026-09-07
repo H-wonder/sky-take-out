874a8dbdd4660eec64bdbd8c7008d698ae31e38c
wonder <2825191095@qq.com>
2026-09-07 15:55:16 +0800

    feat:完成DAY4-1

---
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+import com.sky.dto.SetmealDTO;
+
+public interface SetmealService {
+
+    /**
+     * 新增套餐，同时需要保存菜品和套餐的关联
+     * @param setmealDTO
+     */
+    void saveWithDish(SetmealDTO setmealDTO);
+}
